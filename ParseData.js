6d8c35cbacc8a1a2ffa83ca5f4e6fafc3d19ab38
PinEn Chen <aaaeds@gmail.com>
2015-10-23 17:25:02 -0600

    Update ParseData.js
    add parse

--- ParseData.js
+++ ParseData.js
@@ -1,3 +1,12 @@
 function() {
-
+    var file = evt.target.files[0];
+ 
+    Papa.parse(file, {
+      header: true,
+      dynamicTyping: true,
+      complete: function(results) {
+        var data = results;
+        console.log(data);
+      }
+    });
 }
