afc6b3117bf347a80ff681352b3e2e5f9ba9d929
PinEn Chen <aaaeds@gmail.com>
2015-10-23 18:33:08 -0600

    get data function new version

--- ParseData.js
+++ ParseData.js
@@ -1,22 +1,27 @@
-//var keep_fields = ["year", ""];
-function get_data(year, fields, callback) {
-    var file = "data/Annual_All_Utah_Air_Quality_"+year+".csv";
-
-    $.ajax({
-        type: "GET",
-        url: file,
-        dataType: "text",
-        success: function(data) {
-            processData(data, fields, callback);
-        }
+var fields = ["1st Max Value", "2nd Max Value", "Arithmetic Mean", "City Name", "Year"];
+function get_data(county_name, year, field, callback) {
+    var file = "data/data.csv";
+    open_file(file, function (file) {
+        processData(file, county_name, year, field, callback);
     });
 }
-function processData(file, fields, callback) {
+
+function open_file(name, callback) {
+    $.ajax({
+        type: "GET",
+        url: name,
+        dataType: "text",
+        success: callback
+    });
+}
+function processData(file, county_name, year, field, callback) {
     Papa.parse(file, {
         header: true,
         dynamicTyping: true,
         complete: function(results) {
-            results.data = results.data.map(function(obj) {
+            results.data = results.data.filter(function(doctor) {
+                return doctor["County Code"] === county_name && doctor["Year"] === year && doctor["Parameter Name"] == field;
+            }).map(function(obj) {
                 var newObj = {};
                 for (var key in fields) {
                     newObj[fields[key]] = obj[fields[key]];
