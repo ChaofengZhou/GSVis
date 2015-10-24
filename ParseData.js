57839909af6bb131a328a039eb1d0448e2d93a8d
PinEn Chen <aaaeds@gmail.com>
2015-10-23 19:34:05 -0600

    get data max min function

--- ParseData.js
+++ ParseData.js
@@ -1,4 +1,26 @@
 var fields = ["1st Max Value", "2nd Max Value", "Arithmetic Mean", "City Name", "Year", "1st Max DateTime", "County Name"];
+function get_data_max_min(field, callback) {
+    var file = "data/data.csv";
+    open_file(file, function (file) {
+        processMaxMinData(file, field, callback);
+    });
+}
+function processMaxMinData(file, field, callback){
+    Papa.parse(file, {
+        header: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            var max = -1, min = 100;
+            results.data.filter(function(doctor) {
+                return doctor["Parameter Name"] === field;
+            }).forEach(function(obj) {
+                max = Math.max(max, obj["Arithmetic Mean"]);
+                min = Math.min(min, obj["Arithmetic Mean"]);
+            });
+            callback(max, min);
+        }
+    });
+}
 function get_data(county_name, year, field, callback) {
     var file = "data/Annual_All_Utah_Air_Quality_"+year+".csv";
     open_file(file, function (file) {
