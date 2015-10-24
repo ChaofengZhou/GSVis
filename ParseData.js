09269ba59b1b6b62b177369c9bffda81eea5810c
PinEn Chen <aaaeds@gmail.com>
2015-10-23 18:00:57 -0600

    get data function

--- ParseData.js
+++ ParseData.js
@@ -1,12 +1,30 @@
-function() {
-    var file = evt.target.files[0];
- 
+//var keep_fields = ["year", ""];
+function get_data(year, fields, callback) {
+    var file = "data/Annual_All_Utah_Air_Quality_"+year+".csv";
+
+    $.ajax({
+        type: "GET",
+        url: file,
+        dataType: "text",
+        success: function(data) {
+            processData(data, fields, callback);
+        }
+    });
+}
+function processData(file, fields, callback) {
     Papa.parse(file, {
-      header: true,
-      dynamicTyping: true,
-      complete: function(results) {
-        var data = results;
-        console.log(data);
-      }
+        header: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            results.data = results.data.map(function(obj) {
+                var newObj = {};
+                for (var key in fields) {
+                    newObj[fields[key]] = obj[fields[key]];
+                }
+                return newObj;
+            });
+            console.log(results);
+            callback(results);
+        }
     });
 }
